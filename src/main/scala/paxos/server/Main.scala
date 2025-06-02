dc9b33c21c973dd2941b6c770d09451d94d225d2
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-02 12:23:57 +0200

    added named args

--- src/main/scala/paxos/server/Main.scala
+++ src/main/scala/paxos/server/Main.scala
@@ -1,27 +1,42 @@
 package paxos.server
 
-import paxos.config.JsonLoader
+import paxos.config.{JsonLoader, Peer}
+import scopt.OParser
+
+case class CmdArgs(name: Int = 1, configPath: String = "src/main/resources/config.json")
 
 object Main {
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      println("Usage: ./server <name> <config-path>")
-      System.exit(-1)
+    val builder = OParser.builder[CmdArgs]
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("server"),
+        head("Paxos Server", "1.0"),
+        opt[Int]("name")
+          .action((x, c) => c.copy(name = x))
+          .text("server id name as integer"),
+        opt[String]("config")
+          .action((x, c) => c.copy(configPath = x))
+          .text("path to config.json")
+      )
     }
 
-    val id = args(0).toInt
-    val configPath = args(1)
+    OParser.parse(parser, args, CmdArgs()) match {
+      case Some(cmdArgs) =>
+        val config = JsonLoader.load(cmdArgs.configPath)
+        val maybeSelf = config.peers.find(_.name == cmdArgs.name)
 
-    val config = JsonLoader.load(configPath)
-    val maybeSelf = config.peers.find(_.name == id)
+        if (maybeSelf.isEmpty) {
+          println(s"No peer named ${cmdArgs.name} found in config")
+          System.exit(-1)
+        }
 
-    if (maybeSelf.isEmpty) {
-      println(s"No peer named $id found in config")
-      System.exit(-1)
+        val port = maybeSelf.get.address.split(":")(1).toInt
+        new Server(port).start()
+
+      case None =>
+        System.exit(1)
     }
-
-    val port = maybeSelf.get.address.split(":")(1).toInt
-    val server = new Server(port)
-    server.start()
   }
 }
