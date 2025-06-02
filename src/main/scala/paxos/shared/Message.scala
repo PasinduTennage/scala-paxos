6045e72ba827b4398a011aa8461952ad79c157a9
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-02 12:45:04 +0200

    paxos message types

--- src/main/scala/paxos/shared/Message.scala
+++ src/main/scala/paxos/shared/Message.scala
@@ -2,8 +2,64 @@ package paxos.shared
 
 import upickle.default._
 
-case class Message(senderId: Int, messages: List[String])
+case class Prepare(
+                    senderId: Int,
+                    instance: Int,
+                    prepareBallot: Int)
 
-object Message {
-  implicit val rw: ReadWriter[Message] = macroRW
+object Prepare {
+  implicit val rw: ReadWriter[Prepare] = macroRW
+}
+
+case class Promise(
+                    senderId: Int,
+                    instance: Int,
+                    promiseBallot: Int,
+                    lastAcceptedBallot: Int,
+                    lastAcceptedValue: List[String])
+
+object Promise {
+  implicit val rw: ReadWriter[Promise] = macroRW
+}
+
+case class Propose(
+                    instance: Int,
+                    proposeBallot: Int,
+                    proposeValue: List[String])
+
+object Propose {
+  implicit val rw: ReadWriter[Propose] = macroRW
+}
+
+case class Accept(
+                   instance: Int,
+                   acceptBallot: Int)
+
+object Accept {
+  implicit val rw: ReadWriter[Accept] = macroRW
+}
+
+case class Decide(
+                   instance: Int,
+                   decideBallot: Int)
+
+object Decide {
+  implicit val rw: ReadWriter[Decide] = macroRW
+}
+
+case class FetchRequest(
+                         instance: Int,
+                         decideBallot: Int)
+
+object FetchRequest {
+  implicit val rw: ReadWriter[FetchRequest] = macroRW
+}
+
+case class FetchResponse(
+                          instance: Int,
+                          decideBallot: Int,
+                          value: List[String])
+
+object FetchResponse {
+  implicit val rw: ReadWriter[FetchResponse] = macroRW
 }
