2710891b7ce98f53df598f8156a22cba3d72f5cf
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-06 20:07:06 +0200

    checked Slot classd

--- src/main/scala/paxos/paxos/Paxos.scala
+++ src/main/scala/paxos/paxos/Paxos.scala
@@ -1,5 +1,38 @@
 package paxos.paxos
 
+import paxos.shared.ReplicaBatch
+
+// slot is a single entry in the replicated log
+class Slot {
+  var index: Int = -1
+
+  var decided: Boolean = false
+  var decided_id: String = null
+
+  var prepared_ballot: Int = -1
+  var highest_seen_accepted_ballot: Int = -1
+  var highest_seen_accepted_value: String = null
+  var num_prepare_reponses: Int = 0
+
+  var proposed_ballot: Int = -1
+  var proposed_value_id: String = null
+  var num_accept_reponses: Int = 0
+
+  var accepted_ballot: Int = -1
+  var accepted_value_id: String = null
+}
+
 class Paxos {
-  
+
+  // number of replicas in the system
+  var numReplicas: Int = config.peers.length
+
+  // current view number
+  var view: Int = 0
+
+  // last decided index
+  var lastDecidedIndex: Int = -1
+
+  // replicated log of
+
 }
