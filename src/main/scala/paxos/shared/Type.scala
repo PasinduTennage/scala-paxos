36150135eae4d3d9d51e0a93d7ef396277536b47
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-06 19:56:47 +0200

    checked Type.Scala, added proposal id

--- src/main/scala/paxos/shared/Type.scala
+++ src/main/scala/paxos/shared/Type.scala
@@ -5,57 +5,45 @@ import ujson.Str
 
 sealed trait Message
 
-case class HeartBeat(
-  senderId: Int) extends Message
+case class HeartBeat(senderId: Int) extends Message
 
-case class Id(
-  senderId: Int) extends Message
+case class Id(senderId: Int) extends Message
 
-case class ClientBatch(
-  senderId: Int, commands: List[String]) extends Message
+case class ClientBatch(senderId: Int, commands: List[String]) extends Message
 
-case class ReplicaBatch(
-  proposalId: String, commands: List[ClientBatch]) extends Message  
+case class ReplicaBatch(Id: String, commands: List[ClientBatch]) extends Message
 
-case class Prepare(
-  senderId: Int, 
-  instance: Int, 
-  prepareBallot: Int)extends Message
+case class Prepare(senderId: Int, instance: Int, prepareBallot: Int)
+    extends Message
 
 case class Promise(
     senderId: Int,
     instance: Int,
     promiseBallot: Int,
     lastAcceptedBallot: Int,
-    lastAcceptedValue: List[ClientBatch]
+    lastAcceptedValue: ReplicaBatch
 ) extends Message
 
 case class Propose(
     instance: Int,
     proposeBallot: Int,
-    proposeId: String,
-    proposeValue: List[ClientBatch]) extends Message
-    
+    proposeValue: ReplicaBatch
+) extends Message
 
-case class Accept(
-  instance: Int, 
-  acceptBallot: Int) extends Message
+case class Accept(instance: Int, acceptBallot: Int) extends Message
 
-case class Decide(
-  instance: Int, 
-  decideId: String) extends Message
+case class Decide(instance: Int, Id: String) extends Message
 
-case class FetchRequest(
-  instance: Int, 
-  decideId: String) extends Message
+case class FetchRequest(instance: Int) extends Message
 
 case class FetchResponse(
     instance: Int,
-    decideId: String,
-    value: List[ClientBatch]) extends Message
+    decidedValue: ReplicaBatch,
+) extends Message
 
 object Message {
   implicit val clientBatchRW: ReadWriter[ClientBatch] = macroRW
+  implicit val replicaBatchRW: ReadWriter[ReplicaBatch] = macroRW
   implicit val rw: ReadWriter[Message] = ReadWriter.merge(
     macroRW[HeartBeat],
     macroRW[Id],
