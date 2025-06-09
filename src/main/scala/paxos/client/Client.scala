aeaab4ec9ddbe498ed581a342d4e6cb7993f5a3d
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-09 18:04:55 +0200

    checked Client.scala

--- src/main/scala/paxos/client/Client.scala
+++ src/main/scala/paxos/client/Client.scala
@@ -1,29 +1,38 @@
 package paxos.client
 
 
+import paxos.config.NetworkConfig
+import paxos.shared.Message
+import paxos.shared.ClientBatch
+import paxos.shared.Id
+
+
 import scala.concurrent.ExecutionContext
 import scala.collection.mutable
 import java.io.PrintWriter
 import java.net.Socket
-import paxos.config.NetworkConfig
 import scala.concurrent.Future
 import java.util.concurrent.SynchronousQueue
-import paxos.shared.Message
 import upickle.default._
-import paxos.shared.ClientBatch
+import java.time.LocalDateTime
 
-class Client(name: Int, config: NetworkConfig){
+
+class Client(name: Int, config: NetworkConfig, duration: Int){
     var replicaWriters: mutable.Map[Int, PrintWriter] =
-    mutable.Map.empty // outgoing connection writers for replicas
+    mutable.Map.empty // outgoing connection writers for each replica
 
     implicit val ec: ExecutionContext =
     ExecutionContext.global // global thread pool because this code uses multiple threads
 
-    var inputChannel =
-    new SynchronousQueue[
-      String
-    ]() // central buffer holding all the incoming messages
+    var inputChannel = new SynchronousQueue[String]() // central buffer holding all the incoming client responses
 
+    // sent time stamps
+    var batchSentTime: mutable.Map[String, LocalDateTime] = mutable.Map.empty
+    
+    // received time stamps
+    var batchReceivedTime: mutable.Map[String, LocalDateTime] = mutable.Map.empty
+
+    var batchId: Int = 0
 
     def start(): Unit = {
 
@@ -32,75 +41,151 @@ class Client(name: Int, config: NetworkConfig){
         }
 
         this.connectToReplicas(this.config)
+
+        this.sendRequests()
+
+        Thread.sleep(30000) // wait for 30 seconds to allow all requests to be sent and received
+
+        // compute stats
+        this.computeStats()
         
     }
-    
-    def connectToReplicas(config: NetworkConfig): Unit = {
-    config.peers.foreach { peer =>
-      {
-        var connected = false
 
-        while (!connected) {
-          try {
-            val socket = new Socket(peer.ip, peer.port)
-            val out = new PrintWriter(socket.getOutputStream, true)
-            this.replicaWriters(peer.name) = out
-            val in = new java.io.BufferedReader(
-              new java.io.InputStreamReader(socket.getInputStream)
-            )
-            Future{
-              this.handle_connection(in)
+    def run(): Unit = {
+
+      while (true) {
+
+        val line = this.inputChannel.take()
+
+        val msg = read[Message](line)
+
+        msg match {
+
+          case m: ClientBatch =>
+            // if m.id is not in this.batchReceivedTime, insert
+            if (!this.batchReceivedTime.contains(m.id)) {
+              this.batchReceivedTime(m.id) = LocalDateTime.now()
             }
+        }
+      }
+    }
 
-            println(s"Connected to  $peer")
-            connected = true
-          } catch {
-            case e: Exception =>
-              Thread.sleep(1000)
+    def connectToReplicas(config: NetworkConfig): Unit = {
+      config.peers.foreach { peer =>
+        {
+          var connected = false
+
+          while (!connected) {
+            try {
+              val socket = new Socket(peer.ip, peer.port)
+              val out = new PrintWriter(socket.getOutputStream, true)
+              println(s"Connected to replica ${peer.name} at ${peer.ip}:${peer.port}")
+              // send id
+              val msg = Id(this.name)
+              val json = write[Message](msg)
+              out.println(json)
+
+              println(s"Sent ID to replica ${peer.name}")
+              
+              this.replicaWriters(peer.name) = out
+              
+              val in = new java.io.BufferedReader(
+                new java.io.InputStreamReader(socket.getInputStream)
+              )
+
+              Future{
+                this.handle_connection(in)
+              }
+
+              println(s"Connected to  $peer")
+              connected = true
+            } catch {
+              case e: Exception =>
+                Thread.sleep(1000)
+            }
           }
         }
       }
+      println("connected to all remote replicas")
     }
-    println("connected to all remote replicas")
-  }
 
-  private def handle_connection(in: java.io.BufferedReader): Unit = {
-    while (true) {
-      try {
-        val line = in.readLine()
-        if (line != null) {
-          this.inputChannel.put(line)
-        } else {
-          println("Connection closed by server")
-          return
+    // send requests to replicas
+
+    def sendRequests(): Unit={
+      println(s"Client $name sending requests...")
+      
+      val endTime = LocalDateTime.now().plusSeconds(duration)
+      
+      while (LocalDateTime.now().isBefore(endTime)) {
+        // create a client batch
+
+        val id = s"$name:$batchId"
+        val commands = List.tabulate(50)(i => s"command-$i-${System.currentTimeMillis()}")
+        val batch = ClientBatch(id, this.name, commands)
+
+        batchId += 1
+
+        // send the batch to a random replica
+
+        val replicaIndex = scala.util.Random.nextInt(this.config.peers.length)+1
+        this.replicaWriters(replicaIndex).println(write[Message](batch))
+
+        // update the sent time
+        this.batchSentTime(id) = LocalDateTime.now()
+
+        println(s"Client $name sent batch $id to replica $replicaIndex")
+
+        // wait for a short time before sending the next batch
+        Thread.sleep(100)
+      }
+
+    }
+
+    private def handle_connection(in: java.io.BufferedReader): Unit = {
+      println("Client handling input connection")
+
+      while (true) {
+        try {
+          val line = in.readLine()
+          if (line != null) {
+            this.inputChannel.put(line)
+          } else {
+            println("Connection closed by server")
+            return
+          }
+        } catch {
+          case e: Exception =>
+            println(s"Error reading from connection: ${e.getMessage}")
+            return
         }
-      } catch {
-        case e: Exception =>
-          println(s"Error reading from connection: ${e.getMessage}")
-          return
       }
     }
-  }
 
-  // main event processing loop
 
-  def run(): Unit = {
+    // compute stats after the duration is over
 
-    while (true) {
+    def computeStats(): Unit = {
+      println(s"Client $name computing stats...")
+      
+      var totalReqests = 0
+      var totalLatency:Long = 0
 
-      val line = this.inputChannel.take()
-
-      val msg = read[Message](line)
-
-      msg match {
-
-        case m: ClientBatch =>
-          // TODO
-
+      this.batchSentTime.foreach { case (id, sentTime) =>
+        if (this.batchReceivedTime.contains(id)) {
+          val receivedTime = this.batchReceivedTime(id)
+          val latency = java.time.Duration.between(sentTime, receivedTime).toMillis
+          totalReqests += 50 // fixed number of commands per batch
+          totalLatency += latency
+        }
       }
+
+      // computer throughpt and average latency
+      val throughput = totalReqests.toDouble / duration
+      val averageLatency = if (totalReqests > 0) totalLatency.toDouble / totalReqests else 0.0
+
+      println(s"Client $name stats:")
+      println(s"Average latency: $averageLatency ms")
+      println(s"Throughput: $throughput requests/sec")
     }
-  }
-
-
 
 }
--- src/main/scala/paxos/client/Main.scala
+++ src/main/scala/paxos/client/Main.scala
@@ -10,6 +10,7 @@ import paxos.config.JsonLoader
 case class ClientCmdArgs(
     name: Int = 1,
     configPath: String = "config/config.json",
+    duration: Int = 60 // duration in seconds for which the client will run
 )
 
 object Main {
@@ -25,25 +26,25 @@ object Main {
         head("Client", "1.0"),
         opt[Int]("name")
           .action((x, c) => c.copy(name = x))
-          .text("server name name as integer"),
+          .text("server name as integer"),
         opt[String]("configPath")
           .action((x, c) => c.copy(configPath = x))
-          .text("path to config.json")
+          .text("path to config.json"),
+        opt[Int]("duration")
+          .action((x, c) => c.copy(duration = x))
+          .text("server duration as integer")
       )
     }
 
     OParser.parse(parser, args, ClientCmdArgs()) match {
       case Some(clientCmdArgs) =>
         val config = JsonLoader.load(clientCmdArgs.configPath)
-        val client = new Client(clientCmdArgs.name, config)
+        val client = new Client(clientCmdArgs.name, config, clientCmdArgs.duration)
         client.start()       
 
       case None =>
         println("failed passing args")
         System.exit(1)
     }
-  }
-
-
-  
+  }  
 }
